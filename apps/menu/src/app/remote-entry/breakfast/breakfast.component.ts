4de09e30e19f2d9f8c5abd5e2cbebd3103878ba7
Jason Neal <jasonneal@rocketmortgage.com>
2023-04-14 18:57:32 -0400

    npx nx generate @nrwl/angular:component --project menu remote-entry/breakfast

---
+++ apps/menu/src/app/remote-entry/breakfast/breakfast.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-grill-breakfast',
+  templateUrl: './breakfast.component.html',
+  styleUrls: ['./breakfast.component.scss'],
+})
+export class BreakfastComponent {}
--- apps/menu/src/app/remote-entry/entry.module.ts
+++ apps/menu/src/app/remote-entry/entry.module.ts
@@ -5,9 +5,10 @@ import { RouterModule } from '@angular/router';
 import { RemoteEntryComponent } from './entry.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { remoteRoutes } from './entry.routes';
+import { BreakfastComponent } from './breakfast/breakfast/breakfast.component';
 
 @NgModule({
-  declarations: [RemoteEntryComponent, NxWelcomeComponent],
+  declarations: [RemoteEntryComponent, NxWelcomeComponent, BreakfastComponent],
   imports: [CommonModule, RouterModule.forChild(remoteRoutes)],
   providers: [],
 })
